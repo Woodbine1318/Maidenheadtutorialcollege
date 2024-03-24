7da3d54acb2a228f009d241ae6200638201d5254
Luis Fernando Saavedra Meza <fernando@fsvdr.me>
2024-03-24 12:17:46 -0600

    feat: Add play option to marquee

--- components/ImageMarquee.tsx
+++ components/ImageMarquee.tsx
@@ -3,6 +3,7 @@ import { ImageMarqueeEntry } from '../types';
 import Marquee from 'react-fast-marquee';
 import { parseContentImage } from '@lib/contentful/asset';
 import classNames from 'classnames';
+import Container from './Container';
 
 const ImageMarquee: FC<{ section: ImageMarqueeEntry }> = ({ section: { fields } }) => {
   return (
@@ -13,24 +14,45 @@ const ImageMarquee: FC<{ section: ImageMarqueeEntry }> = ({ section: { fields } 
         'pb-12': fields.spacing === 'Bottom',
       })}
     >
-      <Marquee autoFill>
-        {fields.images.map((image) => {
-          const img = parseContentImage(image);
+      {fields.play ? (
+        <Marquee>
+          {fields.images.map((image) => {
+            const img = parseContentImage(image);
 
-          return (
-            <img
-              src={img!.src}
-              width={img!.width}
-              height={img!.height}
-              alt={img!.alt}
-              loading="eager"
-              srcSet={`${img!.src}?w=300 1x, ${img!.src} 2x`}
-              className="w-full h-24 object-contain mx-4 lg:mx-8 lg:h-28"
-              key={img!.src}
-            />
-          );
-        })}
-      </Marquee>
+            return (
+              <img
+                src={img!.src}
+                width={img!.width}
+                height={img!.height}
+                alt={img!.alt}
+                loading="eager"
+                srcSet={`${img!.src}?w=300 1x, ${img!.src} 2x`}
+                className="w-full h-24 object-contain mx-4 lg:mx-8 lg:h-28"
+                key={img!.src}
+              />
+            );
+          })}
+        </Marquee>
+      ) : (
+        <Container className="flex justify-center items-stretch">
+          {fields.images.map((image) => {
+            const img = parseContentImage(image);
+
+            return (
+              <img
+                src={img!.src}
+                width={img!.width}
+                height={img!.height}
+                alt={img!.alt}
+                loading="eager"
+                srcSet={`${img!.src}?w=300 1x, ${img!.src} 2x`}
+                className="w-full h-24 object-contain lg:h-28"
+                key={img!.src}
+              />
+            );
+          })}
+        </Container>
+      )}
     </section>
   );
 };
