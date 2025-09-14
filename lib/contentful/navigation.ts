1a1c15878ddbdc8a33da430d026a29a46e860de8
Luis Fernando Saavedra Meza <fernando@fsvdr.me>
2025-09-13 19:39:28 -0600

    fix: Undefined error

--- lib/contentful/navigation.ts
+++ lib/contentful/navigation.ts
@@ -5,7 +5,7 @@ import { ContentLink, ContentNavigation, NavigationEntry } from '../../types';
 import { parseLink } from './link';
 
 export const parseNavigation = (entry: UnresolvedLink<'Entry'> | NavigationEntry): ContentNavigation | null => {
-  if (!('fields' in entry)) {
+  if (!entry || !('fields' in entry)) {
     return null;
   }
 
